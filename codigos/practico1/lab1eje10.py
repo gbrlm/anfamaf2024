df6d29b6bf9da79e68ef2b67a31cee7d701a1dff
gbrlm <66442637+gbrlm@users.noreply.github.com>
2024-04-11 09:42:08 -0300

    Add files via upload

---
+++ codigos/practico1/lab1eje10.py
@@ -0,0 +1,13 @@
+# laboratorio 1 - ejercicio 10
+def son_reciprocos(x, y):
+    if abs(x * y - 1) < 1e-7:
+        return True
+    else:
+        return False
+
+import random
+for _ in range(100):
+    x = 1 + random.random()
+    y = 1 / x
+    if not son_reciprocos(x, y):
+        print(x)
